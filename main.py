945b9324c1ba3e4b00b12f685a7fa9f8bdb12703
yudel <jaquan.code@gmail.com>
2026-03-05 22:26:01 +0200

    Finish basic mechanics (add watermark to image, save image)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw, ImageFont
 import os
 import ctypes
 
-
+image = None
 def imageuploader():
+    global image
     filetypes = [("Image files", "*.png;*.jpg;*.jpeg")]
     try:
         path = filedialog.askopenfilename(filetypes=filetypes)
@@ -18,7 +19,10 @@ def imageuploader():
             return
 
         image = Image.open(path)
-        image.thumbnail((400, 400))
+        w, h = image.size
+        new_h = 250
+        new_w = int(w * new_h / h)
+        image = image.resize((new_w, new_h))
         tk_img = ImageTk.PhotoImage(image)
         img_display_label.config(image=tk_img)
         img_display_label.image = tk_img
@@ -27,18 +31,48 @@ def imageuploader():
     except Exception as e:
         messagebox.showerror("Error", f"Failed to load file:\n{e}")
 
+def add_watermark():
+    global image
+    if not image:
+        messagebox.showerror("Error", "No image was selected.")
+    else:
+        draw = ImageDraw.Draw(image)
+        watermark_text = "James' work"
+        w, h = image.size
+        x, y = int(w/2), int(h/2)
+        if x > y:
+            font_size = y
+        else:
+            font_size = x
+        position = (0, 0)
+        draw.text(position, watermark_text, fill=(0, 0, 0), font=ImageFont.truetype("arial.ttf", int(font_size/6)))
+        tk_img = ImageTk.PhotoImage(image)
+        img_display_label.config(image=tk_img)
+        img_display_label.image = tk_img
+
+def save_image():
+    global image
+    if not image:
+        messagebox.showerror("Error", "No image was selected.")
+    else:
+        save_img = filedialog.asksaveasfilename(defaultextension=".jpg")
+        image.save(save_img)
+        messagebox.showinfo("Success", "Image was created successfully.")
+
 ctypes.windll.shcore.SetProcessDpiAwareness(1) # make window look sharp and not blurry
 root = tk.Tk()
 root.title("Image watermarking")
 root.geometry("600x600")
-frame = ttk.Frame(root, padding=10)
-frame.grid()
-heading_label = ttk.Label(frame, text="To imprint a watermark on an image, upload image here.", font=("Arial", 10))
-heading_label.grid(column=0, row=0)
-upload_btn = ttk.Button(frame, text="Browse files", command=imageuploader)
-upload_btn.grid(column=0, row=1)
-file_name_label = ttk.Label(frame, text="", font=("Arial", 10))
-file_name_label.grid(column=0, row=2)
-img_display_label = ttk.Label(frame)
-img_display_label.grid(column=0, row=3)
+heading_label = ttk.Label(root, text="To imprint a watermark on an image, upload image here.", font=("Arial", 10))
+heading_label.grid(column=0, row=0, columnspan=3)
+upload_btn = ttk.Button(root, text="Browse files", command=imageuploader, width=25)
+upload_btn.grid(column=1, row=1)
+file_name_label = ttk.Label(root, text="", font=("Arial", 10))
+file_name_label.grid(column=1, row=2)
+img_display_label = ttk.Label(root)
+img_display_label.grid(column=1, row=3)
+add_water_btn = ttk.Button(root, text="Add watermark", command=add_watermark, width=25)
+add_water_btn.grid(column=1, row=4)
+save_btn = ttk.Button(root, text="Save", command=save_image, width=25)
+save_btn.grid(column=1, row=5)
 root.mainloop()
